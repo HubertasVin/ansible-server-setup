98caf3ba03c09b1860a0af9a5b6714d0a921ed97
HubertasVin <hubertas.personal@proton.me>
2025-03-26 22:57:04 +0200

    UPDATE: Moved passwords to secrets file; modified Obsidian notes backup script.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ansible script to setup a VPS for syncing Obsidian notes and backups
+
+To run the script, use the following command:
+`ansible-playbook playbook.yml --ask-vault-pass`{:.bash}
+
--- sync-data.sh
+++ sync-data.sh
@@ -2,10 +2,23 @@
 
 USER_HOME="/home/insert-user-name-here"
 GITHUB_TOKEN="replace-with-github-token"
-REPO_URL="https://${GITHUB_TOKEN}@github.com/HubertasVin/obsidian-sync.git"
+REPO_URL="https://backupuser:${GITHUB_TOKEN}@github.com/HubertasVin/obsidian-sync.git"
+
+if [ "$GITHUB_TOKEN" = "replace-with-github-token" ]; then
+    echo "Error: GITHUB_TOKEN is not set. Aborting." >&2
+    exit 1
+fi
 
 exec 2>$USER_HOME/sync-data.log
 cd $USER_HOME/dav_data/Obsidian\ Vault || exit 1
+
+# Initialize git repository if not already initialized
+if [ ! -d ".git" ]; then
+    git init
+    git branch -m master main >> /dev/null 2>&1
+    git remote add origin "$REPO_URL"
+    git config --global init.defaultBranch main
+fi
 
 # Mark the directory as safe for Git
 git config --global --add safe.directory $USER_HOME/dav_data/Obsidian\ Vault
@@ -14,5 +27,6 @@ git config --global --add safe.directory $USER_HOME/dav_data/Obsidian\ Vault
 git add -A
 if ! git diff --cached --quiet; then
     git commit -m "Daily sync $(date +'%Y-%m-%d %H:%M:%S')"
-    git push "$REPO_URL" "$(git branch --show-current)" >> $USER_HOME/sync-data.log 2>&1
 fi
+git pull --rebase origin "$(git branch --show-current)" 2>&1
+git push -u origin "$(git branch --show-current)" 2>&1
